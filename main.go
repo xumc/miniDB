954be3662aac5f28db4bc4ab2b76308ef831545b
mxu <mxu@freewheel.tv>
2018-11-07 22:35:36 +0800

    refect ut

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -17,100 +16,5 @@ func main() {
 	}
 	logger := log.New(logFile, "[Debug]", log.LstdFlags)
 
-	s := store.NewStore(logger)
-
-	s.RegisterTable(store.TableDesc{
-		Name: "student",
-		Columns: []store.Column{
-			store.Column{Name: "id", Type: store.ColumnTypeInteger, PrimaryKey: true},
-			store.Column{Name: "name", Type: store.ColumnTypeString},
-			store.Column{Name: "pass", Type: store.ColumnTypeBool},
-		},
-	})
-
-	fmt.Println("----------------insert------------------")
-	r1 := store.Record{
-		TableName: "student",
-		Values:    []interface{}{int64(1), "Jack 1", true},
-	}
-	r2 := store.Record{
-		TableName: "student",
-		Values:    []interface{}{int64(2), "Jack 2", false},
-	}
-	insertRecord(s, r1)
-	insertRecord(s, r2)
-	selectRecords(s, []store.QueryItem{})
-
-	fmt.Println("----------------update------------------")
-
-	updateFn := func(r store.Record) (interface{}, error) {
-		return "prefix_" + r.Values[1].(string), nil
-	}
-
-	updateRecords(
-		s,
-		[]store.QueryItem{
-			store.QueryItem{Key: "id", Operator: store.QueryOperatorEqual, Value: int64(2)},
-		},
-		[]store.UpdateSetItem{
-			store.UpdateSetItem{Name: "name", Value: updateFn},
-		},
-	)
-	selectRecords(s, []store.QueryItem{})
-
-	fmt.Println("----------------delete------------------")
-	deleteRecords(s, []store.QueryItem{
-		store.QueryItem{Key: "pass", Operator: store.QueryOperatorEqual, Value: true},
-	})
-	selectRecords(s, []store.QueryItem{})
-
-	fmt.Println("finish")
-}
-
-func updateRecords(s store.Store, qs []store.QueryItem, uis []store.UpdateSetItem) {
-	affectedRows, err := s.Update("student", qs, uis)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("updated: ", affectedRows)
-}
-
-func deleteRecords(s store.Store, qs []store.QueryItem) {
-	affectedRows, err := s.Delete("student", qs)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("deleted: ", affectedRows)
-}
-
-func selectRecords(s store.Store, qs []store.QueryItem) {
-	rs, err := s.Select("student", qs)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, r := range rs {
-		fmt.Println(r.Values)
-	}
-
-	return
-}
-
-func insertRecord(s store.Store, record store.Record) {
-	affectedRows, err := s.Insert(record.TableName, record)
-	if err != nil {
-		if err == store.ErrDuplicatedRecord {
-			fmt.Println("dumplicated record found in db")
-			return
-		}
-		if err == store.ErrBoolNotSupportPrimaryKey {
-			fmt.Println("bool type doesn't support primary key setting")
-		}
-
-		panic(err)
-	}
-
-	fmt.Println("insert : ", affectedRows)
+	store.NewStore(logger)
 }
---
+++ store/store_test.go
@@ -0,0 +1,130 @@
+package store
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestCRUD(t *testing.T) {
+	fileName := "miniDB.log"
+	logFile, err := os.Create(fileName)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+	}
+	logger := log.New(logFile, "[Debug]", log.LstdFlags)
+
+	s := NewStore(logger)
+
+	fmt.Println("----------------regiester------------------")
+
+	err = s.RegisterTable(TableDesc{
+		Name: "student",
+		Columns: []Column{
+			Column{Name: "id", Type: ColumnTypeInteger, PrimaryKey: true},
+			Column{Name: "name", Type: ColumnTypeString},
+			Column{Name: "pass", Type: ColumnTypeBool},
+		},
+	})
+
+	if err != nil {
+		t.Fatalf("err should be nil, but %s", err)
+	}
+
+	fmt.Println("----------------insert------------------")
+	r1 := Record{
+		TableName: "student",
+		Values:    []interface{}{int64(1), "Jack 1", true},
+	}
+	r2 := Record{
+		TableName: "student",
+		Values:    []interface{}{int64(2), "Jack 2", false},
+	}
+	insertRecord(s, r1)
+	insertRecord(s, r2)
+	assertRecords(s, t, [][]interface{}{
+		[]interface{}{int64(1), "Jack 1", true},
+		[]interface{}{int64(2), "Jack 2", false},
+	})
+
+	fmt.Println("----------------update------------------")
+
+	updateFn := func(r Record) (interface{}, error) {
+		return "prefix_" + r.Values[1].(string), nil
+	}
+
+	updateRecords(
+		s,
+		[]QueryItem{
+			QueryItem{Key: "id", Operator: MatcherEqual, Value: int64(2)},
+		},
+		[]UpdateSetItem{
+			UpdateSetItem{Name: "name", Value: updateFn},
+		},
+	)
+	assertRecords(s, t, [][]interface{}{
+		[]interface{}{int64(1), "Jack 1", true},
+		[]interface{}{int64(2), "prefix_Jack 2", false},
+	})
+
+	fmt.Println("----------------delete------------------")
+	deleteRecords(s, []QueryItem{
+		QueryItem{Key: "pass", Operator: MatcherEqual, Value: true},
+	})
+	assertRecords(s, t, [][]interface{}{
+		[]interface{}{int64(2), "prefix_Jack 2", false},
+	})
+}
+
+func insertRecord(s Store, record Record) {
+	affectedRows, err := s.Insert(record.TableName, record)
+	if err != nil {
+		if err == ErrDuplicatedRecord {
+			fmt.Println("dumplicated record found in db")
+			return
+		}
+		if err == ErrBoolNotSupportPrimaryKey {
+			fmt.Println("bool type doesn't support primary key setting")
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("insert : ", affectedRows)
+}
+
+func assertRecords(s Store, t *testing.T, expected [][]interface{}) {
+	rs, err := s.Select("student", []QueryItem{})
+	if err != nil {
+		panic(err)
+	}
+
+	for i, r := range rs {
+		for j := range r.Values {
+			if rs[i].Values[j] != expected[i][j] {
+				t.Fatalf("value should be %v, but got %v", expected[i][j], rs[i].Values[j])
+			}
+		}
+	}
+	return
+}
+
+func deleteRecords(s Store, qs []QueryItem) {
+	affectedRows, err := s.Delete("student", qs)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("deleted: ", affectedRows)
+}
+
+func updateRecords(s Store, qs []QueryItem, uis []UpdateSetItem) {
+	affectedRows, err := s.Update("student", qs, uis)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("updated: ", affectedRows)
+}
